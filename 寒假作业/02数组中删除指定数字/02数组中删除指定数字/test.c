a0462a974d4af9b62f75b2fb58faa3f40f2c6c36
gutingyue <1335282635@qq.com>
2020-02-09 22:18:26 +0800

    删除指定数字

--- 寒假作业/02数组中删除指定数字/02数组中删除指定数字/test.c
+++ 寒假作业/02数组中删除指定数字/02数组中删除指定数字/test.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <Windows.h>
 
+int removeElement(int* nums, int numsSize, int val){
+	int up = 0;
+	int down = 0;
+	for (; up < numsSize; up++){
+		down = up + 1;
+		if ((nums[up] == val)&&(down<(numsSize-1))){
+			for (; nums[down] == val;){
+				if (down>(numsSize-2)){
+					return up;
+				}
+				down++;
+			}
+			nums[up] = nums[down];
+			nums[down] = val;
+		}
+	}
+	return up;
+}
+
+
 int main(){
-	int nums[] = { 1, 2, 3, 4, 3, 5, 3, 3, 6 };
-	int val = 0;
-
-
-
+	int nums[] = { 0, 1, 2, 2, 3, 0, 4, 2 };
+	int numsSize = sizeof(nums) / sizeof(nums[0]);
+	int val = 2;
+	int newSize = removeElement(nums, numsSize, val);
+	printf("newSize:%d \n", newSize);
+	int i = 0;
+	for (; i < newSize; i++){
+		printf("%d ", nums[i]);
+	}
 	system("pause");
 	return 0;
 }
